ed2442ce3990f0a2d8cf7af6276951d22dcaf872
NicolaRorato <nicola.rorato@gmail.com>
2019-11-02 17:17:42 +0100

    First commit

---
+++ TrialMode.py
@@ -0,0 +1,11 @@
+import os
+import time
+import datetime
+
+if datetime.datetime.now()>datetime.datetime.strptime('01-01-1970','%d-%m-%Y'):
+    print('\n\nTRIAL EXPIRED\n\n')
+    time.sleep(5)
+    quit()
+
+else:
+    pass
